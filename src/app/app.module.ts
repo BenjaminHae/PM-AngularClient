e56158d2495bae5493956d0ddf80b1b5fd2e45ef
Benjamin Häublein <benjaminh@alpine1>
2018-11-09 22:00:52 +0100

    add passwordList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordListComponent } from './password-list/password-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordListComponent
   ],
   imports: [
     BrowserModule,
