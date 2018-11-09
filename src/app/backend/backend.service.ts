a9ed79ff568a9cade82d4d40ad9e725f57ea1098
Benjamin Häublein <benjaminh@alpine1>
2018-11-09 22:00:26 +0100

    added dependencies to backend.service

--- src/app/backend/backend.service.ts
+++ src/app/backend/backend.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AccountBackend } from './backend.js';
+import { EncryptionWrapper } from './cryptoWrapper.js';
+import { Account } from './account.js';
 
 @Injectable({
   providedIn: 'root'
