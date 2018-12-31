b6e3d3b6dbe1f994da80f17340f31ac7fb1636e6
Benjamin Häublein <benjaminh@debian.vm.hp>
2018-12-31 15:19:26 +0100

    add more touchid functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,37 @@ import { TouchidService } from './touchid.service';
 export class AppComponent implements OnInit {
   title = 'passwordManager';
   touchIdAvailable: boolean = false;
+  message: String = "";
 
   constructor(private touchidService: TouchidService) { }
 
   ngOnInit() {
     if (this.touchidService.is_installed()) {
       this.touchidService.available()
-        .then(() => { this.touchIdAvailable = true; })
+        .then(() => { 
+		this.touchIdAvailable = true; 
+	})
     }
+  }
+  prepareKeys(): void {
+	this.touchidService.hasKey("account")
+		.then(() => {
+			this.touchidService.getKey("account", "trying to access the master key")
+				.then((key) => {
+					this.message = "master key is " + key;
+				})
+				.catch((err) => {
+					this.message = "accessing master key failed: " + err;
+				});
+		})
+		.catch(() => {
+			this.touchidService.saveKey("account", "test")
+				.then(() => {
+					this.message = "stored the master key";
+				})
+				.catch((err) => {
+					this.message = "storing master key failed: " + err;
+				});
+		});
   }
 }
