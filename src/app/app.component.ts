c728195f1c4d5d641f145f6a01ff08e1e129e7a4
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-01-01 12:34:38 +0100

    use touchId and show values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
       this.touchidService.available()
         .then(() => { 
 		this.touchIdAvailable = true; 
+		this.prepareKeys();
 	})
     }
   }
