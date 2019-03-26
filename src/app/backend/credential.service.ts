9dcf83702d1d6a2874c160466b8a11cb5b06c258
Benjamin Häublein <benjaminhaeublein@gmail.com>
2019-03-26 18:54:31 +0100

    implemented crypto

--- src/app/backend/credential.service.ts
+++ src/app/backend/credential.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@angular/core';
+//todo make ciphers configurable
 
 @Injectable({
   providedIn: 'root'
 })
 export class CredentialService {
+  private key: CryptoKey;
 
   constructor() { }
 
-  setFromPassword(password: string) {
+  generateFromPassword(password: string): Observable<CryptoKey>{
+    return window.crypto.subtle.generateKey(
+      {
+        name:"AES-GCM", length:256,
+      },
+      false,
+      ["encrypt","decrypt", "wrapKey", "unwrapKey"]
+    )
+    .then((key) => {
+        this.key = key;
+      });
+    //ToDo return result as observable
+  }
+
+  getKey(): CryptoKey {
+    return this.key;
   }
 }
--- src/app/backend/crypto.service.ts
+++ src/app/backend/crypto.service.ts
@@ -7,9 +7,45 @@ export class CryptoService {
 
   constructor(credentials: CredentialService) { }
 
-  decryptChar(ciphertext: string): string { }
+  decryptChar(ciphertext: string, iv: ArrayBuffer): Promise<string> {
+    return window.crypto.subtle.decrypt(
+      {
+        "AES-GCM",
+        iv: iv,
+      }
+      credentials.getKey(),
+      ciphertext
+    )
+    .then((plaintext) => {
+      return this.ab2str(plaintext);
+    });
+  }
 
-  encryptChar(plaintext: string): string { }
+  encryptChar(plaintext: string): Promise<string> {
+    let iv = window.crypto.getRandomValues(new Uint8Array(12));
+    return window.crypto.subtle.encrypt(
+      {
+        "AES-GCM",
+        iv: iv,
+      }
+      credentials.getKey(),
+      this.str2ab(plaintext)
+    )
+    .then((ciphertext) => {
+      return {"ciphertext": Uint8Array(ciphertext), "iv":iv};
+    });
+  }
 
 
+  private function ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+  }
+  private function str2ab(str) {
+    var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i=0, strLen=str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
 }
