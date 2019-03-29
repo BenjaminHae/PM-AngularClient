bf1aefd9bc9b1ed56f3b13e58e92bbdda1114fa5
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-03-29 21:54:32 +0100

    added CryptedObject, removed transformers from Account model

--- src/app/backend/account.ts
+++ src/app/backend/account.ts
@@ -1,12 +1,11 @@
-import { EncryptionWrapper } from './cryptoWrapper';
+import { CryptedObject } from './cryptedObject';
 
 export class Account {
     public index: number;
-    public name: String;
-    public enpassword: String;
-    public other;
+    public name: string;
+    public enpassword: CryptedObject;
+    public other: object;
     public file;
-    public mEncryptionWrapper: EncryptionWrapper;
 
     constructor(index, name, enpassword) {
         this.index = index;
@@ -14,130 +13,27 @@ export class Account {
         this.enpassword = enpassword;
         this.other = {};
         this.file = null;
-        this.mEncryptionWrapper = null;
-    }
-    // reads the account from a encrypted dict
-    static fromEncrypted(encryptionWrapper, encryptedAccount) {
-        return encryptionWrapper.decryptChar(encryptedAccount["name"])
-            .then(function(accountName) {
-                let account = new Account(encryptedAccount["index"], accountName, encryptedAccount["kss"]);
-                return account.setEncryptionWrapper(encryptionWrapper);
-            })
-            .then(function(account){
-                let resultPromise;
-                if (encryptedAccount["additional"] != "") {
-                    //decrypt and extract json
-                    resultPromise = encryptionWrapper.decryptChar(encryptedAccount["additional"])
-                        .then(function(additional) {
-                            let data = JSON.parse(additional);
-                            for (var x in data)
-                                account.setOther(x, data[x]);
-                            return account;
-                        });
-                }
-                else {
-                    resultPromise = Promise.resolve(account);
-                }
-                return resultPromise;
-            });
-    }
-
-    setEncryptionWrapper(wrapper) {
-        var self = this;
-        if (self.encryptionWrapper != null) {
-            var decryptedPassword;
-            var decryptedFileKey;
-            return self.getPassword()
-                .then(function(password) {
-                    decryptedPassword = password;
-                    if (self.hasFile()) {
-                        return self.getFileKey();
-                    }
-                    else {
-                        return "";
-                    }
-                })
-                .then(function(fileKey) {
-                    decryptedFileKey = fileKey;
-                    return;
-                })
-                .then(function(){
-                    self.mEncryptionWrapper = wrapper;
-                    return self.setPassword(decryptedPassword);
-                })
-                .then(function() {
-                    if (decryptedFileKey != '') {
-                        return self.setFileKey(decryptedFileKey);
-                    }
-                    else {
-                        return;
-                    }
-                })
-                .then(function(){
-                    return self;
-                });
-        }
-        else {
-            self.mEncryptionWrapper = wrapper;
-            return Promise.resolve(self);
-        }
-    }
-    get encryptionWrapper() {
-        return this.mEncryptionWrapper;
-    }
-
-    getEncrypted(withFile?){
-        withFile = withFile || false;
-        var self = this;
-        let encryptedResult = { "kss":self.enpassword };
-        if (self.index != null)
-            encryptedResult["index"] = self.index;
-        return self.encryptionWrapper.encryptChar(self.name)
-            .then(function(enName) {
-                encryptedResult["name"] = enName;
-                let other = JSON.stringify(self.other);
-                return self.encryptionWrapper.encryptChar(other);
-            })
-            .then(function(enOther) {
-                encryptedResult["other"] = enOther;
-                if (withFile && self.hasFile()) {
-                    encryptedResult["fk"] = self.file.key;
-                    return self.encryptionWrapper.encryptChar(self.file.name)
-                        .then(function(fname) {
-                            encryptedResult["fname"] = fname;
-                        });
-                }
-            })
-            .then(function(){
-                return encryptedResult;
-            });
     }
 
     get accountName() {
         return this.name;
     }
     setAccountName(name) {
-        var self = this;
-        //reencrypt password
-        return self.getPassword()
-            .then(function(password){;
-                self.name = name;
-                return self.setPassword(password);
-            });
+      this.name = name;
     }
     getPassword(){
-        return this.encryptionWrapper.decryptPassword(this.name, this.enpassword);
+        //return this.encryptionWrapper.decryptPassword(this.name, this.enpassword);
     }
     get password() {
         return this.getPassword();
     }
     setPassword(password){
         var self = this;
-        return this.encryptionWrapper.encryptPassword(this.name, password)
-            .then(function(enPass){
-                self.enpassword = enPass;
-                return enPass;
-            });
+        //return this.encryptionWrapper.encryptPassword(this.name, password)
+        //    .then(function(enPass){
+        //        self.enpassword = enPass;
+        //        return enPass;
+        //    });
     }
     clearOther() {
         this.other = {};
@@ -168,24 +64,13 @@ export class Account {
     addEncryptedFile(name, fkey) {
         var self = this;
         self.file = { "name":"", "key": fkey };
-        return self.encryptionWrapper.decryptChar(name)
-            .then(function(decryptedName) {
-                self.file.name = decryptedName;
-                return self.file;
-            });
+        //return self.encryptionWrapper.decryptChar(name)
+        //    .then(function(decryptedName) {
+        //        self.file.name = decryptedName;
+        //        return self.file;
+        //    });
     }
     hasFile() {
         return 'file' in this;
-    }
-    getFileKey(){
-        return this.encryptionWrapper.decryptPassword(this.file.name, this.file.key);
-    }
-    setFileKey(key){
-        var self = this;
-        return this.encryptionWrapper.encryptPassword(this.file.name, key)
-            .then(function(enKey){
-                self.file.key = enKey;
-                return enKey;
-            });
     }
 }
