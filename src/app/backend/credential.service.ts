b54a9f9deb24f86d2c9cc137dabcc72715ba52d9
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-03-29 19:46:48 +0100

    cleaned up and set to use webcrypto

--- src/app/backend/credential.service.ts
+++ src/app/backend/credential.service.ts
@@ -9,17 +9,34 @@ export class CredentialService {
 
   constructor() { }
 
-  generateFromPassword(password: string): Observable<CryptoKey>{
-    return window.crypto.subtle.generateKey(
-      {
-        name:"AES-GCM", length:256,
-      },
-      false,
-      ["encrypt","decrypt", "wrapKey", "unwrapKey"]
-    )
+  generateFromPassword(password: string): PromiseLike<CryptoKey>{
+    let enc = new TextEncoder();
+    return window.crypto.subtle.importKey(
+        "raw",
+        enc.encode(password),
+        {
+          "name": "PBKDF2",
+        //the next lines are a trick to get the typescript compiler to accept the type :/
+          "generator": new Uint8Array(12),
+          "prime": new Uint8Array(12),
+        },
+        false,
+        ["deriveBits", "deriveKey"]
+        )
+      .then((keyMaterial) => {
+//Todo: create salt
+          return window.crypto.subtle.deriveKey(
+              {"name": "PBKDF2", salt:new ArrayBuffer(0), "iterations": 100000, "hash": "SHA-256" },
+              keyMaterial,
+              { name: "AES-GCM", length:256, },
+              false,
+              ["encrypt","decrypt", "wrapKey", "unwrapKey"]
+              )
+          })
     .then((key) => {
         this.key = key;
-      });
+        return key;
+        });
     //ToDo return result as observable
   }
 
--- src/app/backend/crypto.service.ts
+++ src/app/backend/crypto.service.ts
@@ -1,46 +1,84 @@
 import { Injectable } from '@angular/core';
+import { CredentialService } from './credential.service';
 
 @Injectable({
   providedIn: 'root'
 })
+export class CryptedObject {
+  iv: ArrayBuffer;
+  ciphertext: ArrayBuffer;
+  constructor(iv: ArrayBuffer, ciphertext: ArrayBuffer) {
+    this.iv = iv;
+    this.ciphertext = ciphertext;
+  }
+  static fromBase64JSON(encodedObject: string): CryptedObject {
+    let encObject = JSON.parse(encodedObject);
+    return new CryptedObject(CryptedObject._base64ToArrayBuffer(encObject.iv), CryptedObject._base64ToArrayBuffer(encObject.ciphertext));
+  }
+  toBase64JSON(): string {
+    return JSON.stringify({"iv": CryptedObject._BufferToBase64(this.iv), "ciphertext": CryptedObject._BufferToBase64(this.ciphertext)});
+  }
+  static _BufferToBase64(buffer: ArrayBuffer): string {
+    let binary = '';
+    let bytes = new Uint8Array(buffer);
+    let len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa(binary);
+  }
+  static _base64ToArrayBuffer(base64: string): ArrayBuffer {
+    let binary_string = window.atob(base64);
+    let len = binary_string.length;
+    let bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
+    }
+   return bytes.buffer;
+  }
+}
 export class CryptoService {
 
-  constructor(credentials: CredentialService) { }
+  credentials: CredentialService;
 
-  decryptChar(ciphertext: string, iv: ArrayBuffer): Promise<string> {
+  constructor(credentials: CredentialService) {
+    this.credentials = credentials;
+  }
+
+  decryptChar(crypt: CryptedObject): PromiseLike<string> {
     return window.crypto.subtle.decrypt(
       {
-        "AES-GCM",
-        iv: iv,
-      }
-      credentials.getKey(),
-      ciphertext
+        name: "AES-GCM",
+        iv: crypt.iv,
+      },
+      this.credentials.getKey(),
+      crypt.ciphertext
     )
     .then((plaintext) => {
       return this.ab2str(plaintext);
     });
   }
 
-  encryptChar(plaintext: string): Promise<string> {
+  encryptChar(plaintext: string): PromiseLike<CryptedObject> {
     let iv = window.crypto.getRandomValues(new Uint8Array(12));
     return window.crypto.subtle.encrypt(
       {
-        "AES-GCM",
+        name: "AES-GCM",
         iv: iv,
-      }
-      credentials.getKey(),
+      },
+      this.credentials.getKey(),
       this.str2ab(plaintext)
     )
     .then((ciphertext) => {
-      return {"ciphertext": Uint8Array(ciphertext), "iv":iv};
+      return new CryptedObject(iv, new Uint8Array(ciphertext));
     });
   }
 
 
-  private function ab2str(buf) {
+  private ab2str(buf): string{
     return String.fromCharCode.apply(null, new Uint16Array(buf));
   }
-  private function str2ab(str) {
+  private str2ab(str): ArrayBuffer{
     var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
     var bufView = new Uint16Array(buf);
     for (var i=0, strLen=str.length; i < strLen; i++) {
