594af56547d2127b873d40e268fd8d82044b26ae
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-04-14 14:28:10 +0200

    transfor OpenAPI account to encrytedAccount

--- src/app/backend/api/accounts.service.ts
+++ src/app/backend/api/accounts.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { AccountsService as OpenAPIAccountsService, AccountId } from '@pm-server/pm-server';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { encryptedAccount } from '../models/encryptedAccount';
+import { AccountTransformerService } from '../controller/account-transformer.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
 
-  constructor(private accountsService: OpenAPIAccountsService) { }
+  constructor(private accountsService: OpenAPIAccountsService, private accountTransformer: AccountTransformerService) { }
 
-  getAccounts(): Observable<Array<AccountId>> {
-    return this.accountsService.getAccounts();
+  getAccounts(): Observable<Array<encryptedAccount>> {
+    return this.accountsService.getAccounts()
+      .pipe(
+          map((accounts: Array<AccountId>): Array<encryptedAccount> => {
+            return accounts.map((account: AccountId) => {
+                return this.accountTransformer.encryptedAccountFromOpenAPI(account);
+                });
+            })
+          );
   }
 }
