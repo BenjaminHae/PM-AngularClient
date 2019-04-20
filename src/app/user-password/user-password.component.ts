56f127e21f06505313a1e883ec7e9367438e1288
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-04-20 13:07:37 +0200

    implement functionality in user-password component

--- src/app/user-password/user-password.component.ts
+++ src/app/user-password/user-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../backend/backend.service';
 
 @Component({
   selector: 'app-user-password',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-password.component.css']
 })
 export class UserPasswordComponent implements OnInit {
+  private message: string;
+  private currentPassword: string;
+  private newPassword1: string;
+  private newPassword2: string;
 
-  constructor() { }
+  constructor(private backend: BackendService) { }
 
   ngOnInit() {
   }
 
+  changePassword() {
+    console.log(this.newPassword1);
+    this.backend.changeUserPassword(this.newPassword1)
+      .then((obs)=>{
+          obs
+          .subscribe(()=> {
+              this.message = "Successful, please reload and relogin";
+              });
+          });
+  }
 }
