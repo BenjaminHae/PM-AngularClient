53099cbd9f3af76edc7ad6e1eee0f10ae518b863
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-06-01 19:51:13 +0200

    change edit-account to reactive form

--- src/app/edit-account/edit-account.component.ts
+++ src/app/edit-account/edit-account.component.ts
@@ -4,6 +4,7 @@ import { CryptoService } from '../backend/crypto.service';
 import { AccountTransformerService } from '../backend/controller/account-transformer.service';
 import { Account } from '../backend/models/account';
 import { CryptedObject } from '../backend/models/cryptedObject';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-account',
@@ -11,15 +12,20 @@ import { CryptedObject } from '../backend/models/cryptedObject';
   styleUrls: ['./edit-account.component.css']
 })
 export class EditAccountComponent implements OnInit {
+  editAccountForm = this.fb.group({
+    username: [null, Validators.required],
+    password: [null, Validators.required],
+    other: [null, Validators.required]
+  });
+  private hide: boolean = true;
   private account: Account;
   @Input('updateAccount')
   set updateAccount(value: Account) {
     this.setUpdateAccount(value);
   }
-  private password: string = "";
   private message: string = "";
 
-  constructor(private backend:BackendService, private crypto: CryptoService, private accountTransformer: AccountTransformerService) {
+  constructor(private backend:BackendService, private crypto: CryptoService, private accountTransformer: AccountTransformerService, private fb: FormBuilder) {
   }
 
   ngOnInit() {
@@ -27,36 +33,40 @@ export class EditAccountComponent implements OnInit {
   }
 
   resetForm() {
-    this.password = "";
+    this.clearForm();
     this.account = new Account(null, "", null);
+  }
+
+  clearForm() {
+    this.editAccountForm.controls.username.setValue("");
+    this.editAccountForm.controls.password.setValue("");
+    this.hide = true;
   }
 
   setUpdateAccount(account: Account) {
     this.account = account;
+    this.clearForm();
     if (this.account) {
+      console.log(this.account);
+      console.log(this.editAccountForm.controls);
+      this.editAccountForm.controls.username.setValue(this.account.name);
       console.log("retrieve password");
-      this.accountTransformer.getPassword(this.account)
-        .then((password) => {
-            console.log("write password to update");
-            this.password = password;
-            });
-    }
-    else {
-      this.password = "";
     }
   }
 
   storeAccount() {
+    this.account.name = this.editAccountForm.controls.username.value;
+    //Todo store other
     //Only look at password if it has changed
     let PasswordPromise: PromiseLike<CryptedObject>;
-    if (this.password != "") {
+    if (this.editAccountForm.controls.password.value != "") {
       console.log("passwordChanged");
-      PasswordPromise = this.crypto.encryptChar(this.password);
+      PasswordPromise = this.crypto.encryptChar(this.editAccountForm.controls.password.value);
     }
     else if (!this.account.index) {
       // Todo auto-generate password
       console.log("Todo auto generate password");
-      PasswordPromise = this.crypto.encryptChar(this.password);
+      PasswordPromise = this.crypto.encryptChar(this.editAccountForm.controls.password.value);
     }
     else{
       console.log("keeping old password");
@@ -92,5 +102,16 @@ export class EditAccountComponent implements OnInit {
 
   abort() {
     this.resetForm();
+  }
+
+  passwordToggle() {
+    this.hide = !this.hide;
+    if (!this.hide && this.editAccountForm.controls.password.value == "") {
+      this.accountTransformer.getPassword(this.account)
+        .then((password) => {
+            console.log("write password to update");
+            this.editAccountForm.controls.password.setValue(password);
+            });
+    }
   }
 }
