0d3de99e7c2fe35f8126911afe5e62edca7be083
Benjamin Häublein <benjaminh@debian.vm.hp>
2019-07-06 21:32:05 +0200

    fix animation in account table

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { BackendService } from '../backend/backend.service';
 import { Account } from '../backend/models/account';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-password-list',
   templateUrl: './password-list.component.html',
-  styleUrls: ['./password-list.component.css']
+  styleUrls: ['./password-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class PasswordListComponent implements OnInit {
   @Output() updateAccount = new EventEmitter();
